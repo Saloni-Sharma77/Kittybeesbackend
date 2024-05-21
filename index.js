c5e92bf3bf67d5047832cec3b1c6572a67621f98
saloni <sharmasaloni661@gmail.com>
2024-05-21 19:01:22 +0530

    Kitty_Bee

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+// const path = require('path');
+const port = process.env.PORT || 3000;
+
+const app = express();
+const server = app.listen(port, () => {
+  console.log(`Your server is running on port ${port}`);
+});
+
+
+
+require('./src/config/db');
+const routes = require('./src/routes/routes');
+
+app.use(express.json());
+app.use(cors());
+app.use('/', routes);
+
+
+
+
---
+++ src/routes/routes.js
@@ -0,0 +1,9 @@
+
+const express = require("express");
+const router = express.Router();
+// const user_controller=require("../controller/usercontroller");
+
+// router.post("")
+
+
+module.exports = router;
