21a49ea890cbdda4ec4eef3b04cb09e0c7baa6f3
gurpreet1603 <gsardarji1995@gmail.com>
2024-07-08 16:34:46 +0530

    done

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 4000;
 const app = express();
-// app.use(bodyParser.json());
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Your server is running on port ${port}`);
 });
-
 
 require('./src/config/db');
 const routes = require('./src/routes/routes');
