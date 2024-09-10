9a4dfb5801cefb28e5f15e45b959363d24e7ff5f
Avdheshchoudhary <126864613+Avdheshchoudhary@users.noreply.github.com>
2024-09-10 12:20:46 +0530

    dev

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -92,12 +92,12 @@ router.delete("/deleteColor/:id", color_controller.deleteColor);
 router.patch("/updateColorStatus/:id", color_controller.updateColorStatus);
 
 // // City routes
-// router.get("/getAllCity", city_controller.getAllCity);
-// router.post("/addCity", city_controller.addCity);
-// router.get("/getCityById/:id", city_controller.getCityById);
-// router.put("/updateCity/:id", city_controller.updateCity);
-// router.delete("/deleteCity/:id", city_controller.deleteCity);
-// router.patch("/updateCityStatus/:id", city_controller.updateCityStatus);
+// Route to create a new city
+router.post('/createCity',city_controller.createCity);
+router.get('/getAllCities',city_controller.getAllCities);
+router.get('/getCityById/:id', city_controller.getCityById);
+router.put('/updateCity/:id', city_controller.updateCity);
+router.delete('/deleteCity/:id', city_controller.deleteCity);
 
 //address routes
 router.post('/createAddress', addressController.createAddress);
