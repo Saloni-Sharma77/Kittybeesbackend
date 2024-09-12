f22432122581eb586df27201505b750a8a268a12
Avdheshchoudhary <126864613+Avdheshchoudhary@users.noreply.github.com>
2024-09-12 18:24:00 +0530

    dev

--- src/schema/bookingrequestSchema.js
+++ src/schema/bookingrequestSchema.js
@@ -1,21 +1,21 @@
 const mongoose = require('mongoose');
 
 const bookingRequestSchema = new mongoose.Schema({
-    contactPersonName: { type: String, required: true },
-    emailAddress: { type: String, required: true },
-    contactNumber: { type: String, required: true },
-    date: { type: Date, required: true },
-    time: { type: String, required: true },
-    minimumGuests: { type: Number, required: true },
+    contactPersonName: { type: String,  },
+    emailAddress: { type: String,  },
+    contactNumber: { type: String,  },
+    date: { type: Date, },
+    time: { type: String,  },
+    minimumGuests: { type: Number, },
     vegNonVegDrinks: {
         type: [String],
         enum: ['Veg', 'NonVeg', 'Drinks'],
-        required: true
+        
     },
     preference: {
         type: String,
         enum: ['Buffet', 'AlaCarte', 'MainCourse'],
-        required: true
+        
     },
     starters: { type: [String], default: [] },
     mainCourse: { type: [String], default: [] },
