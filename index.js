11a8719549df44b34303571e869604d84c666437
gurpreet.singh <viddeveloper003@gmail.com>
2024-10-09 14:10:11 +0530

    sockt

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 const server = http.createServer(app);
 
 // Create a WebSocket server
-const wss = new WebSocket.Server({port:8080});
+const wss = new WebSocket.Server({port:3000});
 
 // Socket handler (passing the WebSocket server instance)
 // const socketHandler = require('./src/socket/socket');
