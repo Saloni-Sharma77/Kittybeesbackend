9831b49899d69556be12446d9180fe9760a18917
gurpreet1603 <gsardarji1995@gmail.com>
2024-12-09 16:26:20 +0530

    dev

--- src/controller/client/testController.js
+++ src/controller/client/testController.js
@@ -23,40 +23,58 @@ exports.sendotptest = async (req, res) => {
       userData: userInfo,
     });
   }
+  const checkUserAlreadyLoggedIn = await User.findOne({phoneNumber})
 
-  try {
-    // Make the request to the external API (MessageCentral) for sending OTP
-    const axiosResponse = await axios.post(`https://cpaas.messagecentral.com/verification/v3/send?countryCode=91&customerId=${process.env.MESSAGE_CENTRAL_USER_ID}&flowType=SMS&mobileNumber=${phoneNumber}`, 
-      {},
-      {
-        headers: {
-          'authToken': process.env.MESSAGE_CENTRAL_AUTH_TOKEN
+  if(checkUserAlreadyLoggedIn.verifiedBy == 'notyet'){
+    try {
+      // Make the request to the external API (MessageCentral) for sending OTP
+      const axiosResponse = await axios.post(`https://cpaas.messagecentral.com/verification/v3/send?countryCode=91&customerId=${process.env.MESSAGE_CENTRAL_USER_ID}&flowType=SMS&mobileNumber=${phoneNumber}`, 
+        {},
+        {
+          headers: {
+            'authToken': process.env.MESSAGE_CENTRAL_AUTH_TOKEN
+          }
         }
-      }
-    );
-
-    // Log the response from the external API (for debugging if needed)
-    console.log('Response from MessageCentral:', axiosResponse.data);
-
-    // Filter to update or create user with the phone number
-    const filter = { phoneNumber };
-
-    // Update the user document with the phone number or insert if not found
-    const update = { $set: { phoneNumber } };
-    const options = { upsert: true, new: true };
-   const userInfo = await User.findOneAndUpdate(filter, update, options);
-
-    // Return the actual response from the external API to the client
+      );
+  
+      // Log the response from the external API (for debugging if needed)
+      console.log('Response from MessageCentral:', axiosResponse.data);
+  
+      // Filter to update or create user with the phone number
+      const filter = { phoneNumber };
+  
+      // Update the user document with the phone number or insert if not found
+      const update = { $set: { phoneNumber } };
+      const options = { upsert: true, new: true };
+     const userInfo = await User.findOneAndUpdate(filter, update, options);
+      // Return the actual response from the external API to the client
+      res.status(200).send({
+        success: true,
+        message: 'OTP sent successfully',
+        data: axiosResponse.data , // Send the response data from the API
+        userData: userInfo,
+      });
+    } catch (error) {
+      console.error('Error sending OTP:', error.response ? error.response.data : error.message);
+      res.status(500).send({ error: error.response ? error.response.data : 'Failed to send OTP' });
+    }
+  }else{
+    const token = jwt.sign({ phoneNumber }, process.env.JWT_SECRET, {
+      expiresIn: '1h',
+    });
+    const userInfo = await User.find({phoneNumber});
     res.status(200).send({
       success: true,
-      message: 'OTP sent successfully',
-      data: axiosResponse.data , // Send the response data from the API
+      message: 'OTP verified successfully',
+      token: token,
+      // data :response.data,
       userData: userInfo,
+
     });
-  } catch (error) {
-    console.error('Error sending OTP:', error.response ? error.response.data : error.message);
-    res.status(500).send({ error: error.response ? error.response.data : 'Failed to send OTP' });
+    
   }
+
+ 
 };
 
 
