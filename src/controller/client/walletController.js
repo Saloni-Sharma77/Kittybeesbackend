c5d559fc56b966c2fbd4a47308be1e52d0d942a0
gurpreet1603 <gsardarji1995@gmail.com>
2024-12-26 15:48:01 +0530

    dev

--- src/controller/client/walletController.js
+++ src/controller/client/walletController.js
@@ -316,18 +316,18 @@ exports.getUserWalletForGroup = async (req, res) => {
   const { userId, groupId } = req.params; // Assuming you get these from the request params
 
   try {
-    const walletEntries = await WalletModel.find({
+    const transactions = await WalletModel.find({
       userId: userId,
       groupId: groupId,
     }).populate('kittyId','name image').populate('groupId','name image').populate('userId','fullname');
 
-    if (walletEntries.length === 0) {
+    if (transactions.length === 0) {
       return res.status(404).json({
         message: "No wallet entries found for this user in the group",
       });
     }
 
-    res.status(200).json({ walletEntries });
+    res.status(200).json({ transactions });
   } catch (error) {
     console.error("Error fetching wallet entries:", error.message);
     res.status(500).json({ message: "Server error" });
