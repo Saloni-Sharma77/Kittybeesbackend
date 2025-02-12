66295f2b795ab7ec3ed7b52554d8b3a19f6ffb9f
Mili Shandilya <viddeveloper15@gmail.com>
2025-02-12 12:45:52 +0530

    add commit

--- src/controller/client/groupController.js
+++ src/controller/client/groupController.js
@@ -40,7 +40,7 @@ exports.addGroup = async (req, res) => {
       { name: 'groupFrequencyId', value: groupFrequencyId },
       { name: 'groupCityArea', value: groupCityArea },
       { name: 'contributionAmount', value: contributionAmount },
-      { name: 'image', value: image },
+      // { name: 'image', value: image },
       { name: 'referralCode', value: referralCode }
     ];
 
