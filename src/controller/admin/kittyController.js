2e15120b7e0fe80a8c51c08374e72cd1821457fa
neha harpalani <viddeveloper112@gmail.com>
2025-04-18 13:50:35 +0530

    isrequesteduser field added in join kitty

--- src/controller/admin/kittyController.js
+++ src/controller/admin/kittyController.js
@@ -89,15 +89,7 @@ exports.addKitty = async (req, res) => {
     const group = await GroupSchema.findById(groupId).select("userIds userId name contributionAmount");
 
     // Create members array from userIds
-    const members = group.userIds
-    .filter(user => user.userId.toString() !== userId.toString()) // Exclude the creator
-    .map(user => ({ userId: user.userId, status: "pending" }));
-
-    // Add the admin (group.userId) to members if not already included
-    if (!members.some(member => member.userId.toString() === group.userId.toString())) {
-      members.push({ userId: group.userId, status: "isrequesteduser" }); // Admin has a different status
-    }
-
+  
     // Validate and structure the poll data
     const theamePollData = theamepoll
       ? {
@@ -174,7 +166,6 @@ exports.addKitty = async (req, res) => {
       venuepoll: venuePollData,
       planKittypoll: planKittyPollData,
       activityKittypoll: activityKittyPollData,
-      members,
       tampimage
 
     });
