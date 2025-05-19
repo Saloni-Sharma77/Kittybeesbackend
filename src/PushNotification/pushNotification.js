77e5306803bd8dcece3227e2a4f7f72cd9983897
neha harpalani <viddeveloper112@gmail.com>
2025-05-19 12:38:08 +0530

    added route for chat

--- src/PushNotification/pushNotification.js
+++ src/PushNotification/pushNotification.js
@@ -189,13 +189,21 @@ console.log(responseData,"responseDataresponseData");
             console.log('No valid FCM tokens available for sending notifications.');
             return { message: 'No valid tokens found. No notifications sent.' };
         }
-        // const sanitizedResponseData = {};
+        const sanitizedResponseData = {};
         if (responseData && typeof responseData === 'object') {
             // for (const [key, value] of Object.entries(responseData)) {
             //     sanitizedResponseData[key] = typeof value === 'string' ? value : JSON.stringify(value);
             // }
             for (const [key, value] of Object.entries(responseData)) {
-    sanitizedResponseData[key] = typeof value === 'string' ? value : JSON.stringify(value);
+    if (typeof value === 'string') {
+        sanitizedResponseData[key] = value;
+    } else if (Array.isArray(value)) {
+        sanitizedResponseData[key] = JSON.stringify(value); // Optional: or join(',') if you control both ends
+    } else if (value !== null && typeof value === 'object') {
+        sanitizedResponseData[key] = JSON.stringify(value);
+    } else {
+        sanitizedResponseData[key] = String(value);
+    }
 }
 
         }
